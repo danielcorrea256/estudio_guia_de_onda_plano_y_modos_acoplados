6ed1e70011f5364f2c1f639fba42c66f6882305f
Daniel Correa <dancorreaca@unal.edu.co>
2025-02-12 20:09:59 -0500

    Analisis Ondulatorio

---
+++ metodo_ondulatorio.py
@@ -0,0 +1,127 @@
+import scipy.optimize
+import math
+
+
+def get_W(n_co, n_cl, m, modo):
+    """
+    En la teoria se tiene W = gamma * h / 2
+    Y dependiendo del m y el modo, W se puede expresar como una funcion de U = kappa * h / 2
+    Para resolver las ecuaciones vamos a plantear en terminos de U, por lo que necesitaremos tener W como funcion de U
+
+    Args:
+        n_co (float): Índice de refracción del núcleo de la guía de onda.
+        n_cl (float): Índice de refracción del revestimiento (cladding).
+        m (int): Número de modo de propagación (0, 1, 2, ...).
+        modo (str): Tipo de modo de propagación, puede ser "TE" (Transverse Electric) o "TM" (Transverse Magnetic)
+
+    Return:
+        function: Funcion lambda que representa a W en terminos de U
+
+    Raises:
+        ValueError: Si el modo no es "TE" o "TM".
+    """
+
+    # De la teoria se tienen los siguientes casos
+    if   m%2==0 and modo=="TE":
+        return lambda U : U * math.tan(U)
+    elif m%2==1 and modo=="TE":
+        return lambda U : U / math.tan(U)
+    elif m%2==0 and modo=="TM":
+        return lambda U : (n_cl / n_co)**2 * U * math.tan(U)
+    elif m%2==1 and modo=="TM":
+        return lambda U : (n_cl / n_co)**2 * U * 1/math.tan(U)
+    else:
+        raise ValueError(f"Modo no valido, solo se vale TM o TE, se recibio {modo}")
+
+
+def funcion_ondulatoria(n_co, n_cl, h, k_0, m, modo):
+    """
+    Retorna una funcion cuyas raices son los valores permitidos para U en la propagacion de modos de onda
+    Tenemos en general que debe cumplirse U^2 + W^2 = (k_0 * h / 2)^2 * (n_co^2  - n_cl^2)
+    Pero vamos a obtener W en terminos de U, este nuevo termino depende de m y del modo
+    Y teniendo la ecuacion en terminos de U, vamos a hallar las raices, que son los valores permitidos 
+
+    Args:
+        n_co (float): Índice de refracción del núcleo de la guía de onda.
+        n_cl (float): Índice de refracción del revestimiento (cladding).
+        h (float): Altura del núcleo de la guía de onda.
+        k_0 (float): Número de onda en el vacío.
+        m (int): Número de modo de propagación (0, 1, 2, ...).
+        modo (str): Tipo de modo de propagación, puede ser "TE" (Transverse Electric) o "TM" (Transverse Magnetic).
+
+    Returns:
+        function: Función lambda donde sus raices son los valores permitidos para U
+
+    Raises:
+        ValueError: Si el modo no es "TE" o "TM".
+    """
+
+    # W es una funcion en terminos de U.
+    W = get_W(
+        n_co=n_co, 
+        n_cl=n_cl,
+        m=m,
+        modo=modo
+    )
+
+    left_side_equation = lambda U : (U**2 + W(U)**2) 
+    right_side_equation = (k_0 * math.pi * h / 2)**2 * (n_co**2  - n_cl**2)
+    print(right_side_equation)
+    # Como se tiene de la teoria que left_side_equation = right_side_equation
+    # Entonces left_side_equation - right_side_equation = 0.
+    z = lambda U : left_side_equation(U) - right_side_equation
+    
+    # Luego las raices de z son los valores de U permitidos.
+    return z
+
+
+def metodo_ondulatorio(n_co, n_cl, h, k_0, lambd, ms):
+    """
+    Calcula los ángulos de incidencia permitidos (theta) para los modos TE y TM en una guía de onda
+    utilizando el análisis ondulatorio.
+
+    Args:
+        n_co (float): Índice de refracción del núcleo de la guía de onda.
+        n_cl (float): Índice de refracción del revestimiento (cladding).
+        h (float): Altura del núcleo de la guía de onda.
+        k_0 (float): Número de onda en el vacío.
+        lambd (float): Longitud de onda de la luz en el medio.
+        ms (list[int]): Lista de números de modo a calcular.
+
+    Returns:
+        dict: Un diccionario con los ángulos de incidencia para los modos TE y TM en grados.
+              - 'TE': Diccionario de modos TE con m como clave y theta_m en grados como valor.
+              - 'TM': Diccionario de modos TM con m como clave y theta_m en grados como valor.
+    """
+
+
+    TEs = {} # Diccionario para almacenar los ángulos de incidencia para los modos TE
+    TMs = {} # Diccionario para almacenar los ángulos de incidencia para los modos TM
+
+    for m in ms:
+        # Definir el intervalo de búsqueda para U según la periodicidad de la función tangente
+        l = m*math.pi/2 
+        r = (m+1)*math.pi/2  
+        interval = (l, r) 
+
+        # Resolver para el modo TE
+        # 1. Tenemos una expresion f_TE(U) donde al igualar a 0, tenemos los valores de U permitidos
+        f_TE = funcion_ondulatoria(n_co, n_cl, h, k_0, m, "TE") 
+        
+        # 2. Valor valido para U en la ecuacion original
+        U_TE = scipy.optimize.root_scalar(f_TE, method="bisect", bracket=interval).root # raiz de f_TE
+        
+        # 3. Calcular el angulo de incidencia
+        theta_TE = math.acos((U_TE * lambd) / (math.pi * n_co)) 
+        
+        # 4. Guardar el angulo en grados
+        TEs[m] = math.degrees(theta_TE) 
+
+        # Resolver para el modo TM, de manera similar al modo TE
+        f_TM = funcion_ondulatoria(n_co, n_cl, h, k_0, m, "TM") 
+        U_TM = scipy.optimize.root_scalar(f_TM, method="bisect" ,bracket=interval).root 
+        theta_TM = math.acos((U_TM * lambd) / (math.pi * n_co))
+        TMs[m] = math.degrees(theta_TM)
+
+    # Retornar los ángulos calculados para los modos TE y TM
+    return {'TE': TEs, 'TM': TMs}
---
+++ metodo_ondulatorio_test.py
@@ -0,0 +1,130 @@
+import numpy as np
+import math
+from metodo_ondulatorio import funcion_ondulatoria, metodo_ondulatorio
+
+
+def test_TE_function(): 
+    """
+    Verifica que la función funcion_ondulatoria para los modos TE (Transverse Electric) 
+    genere resultados consistentes con una funcion hallada anteriormente en el Taller de Fotonica y Fibras opticas.
+    Esta funcion es f_TE_reference, y ya se basa en n_co, n_cl, h, k_0 dados
+
+    Se prueba para los tres primeros modos (m = 0, 1, 2).
+
+    Raises:
+        AssertionError: Si los valores calculados no coinciden con la referencia.
+    """
+
+    # Parámetros de la guía de onda
+    n_co = 1.5  # Índice de refracción del núcleo
+    n_cl = 1    # Índice de refracción del revestimiento
+    h = 1       # Altura del núcleo
+    k_0 = 2     # Número de onda en el vacío
+    modo = "TE" # Tipo de modo (Transverse Electric)
+    
+    # Función que se hallo en el Taller de Fotonica y Fibras opticas
+    def f_TE_reference(U, m):
+        if m%2 == 0:
+            return (U*math.tan(U))**2 + U**2 - 1.25*math.pi**2
+        else:
+            return (U/math.tan(U))**2 + U**2 - 1.25*math.pi**2        
+
+    # Conjunto de valores en los que vamos a probar la funcion
+    # se evita el 0 porque se evalua en cotangente de U
+    line = np.linspace(0.001, math.pi/2, 50)
+
+    for m in range(3): 
+        # Obtener la función generada por funcion_ondulatoria
+        f = funcion_ondulatoria(n_co=n_co, n_cl=n_cl, h=h, k_0=k_0, m=m, modo=modo)
+
+        # Evaluar la función generada y la referencia en el mismo rango
+        result = [f(x) for x in line]
+        real = [f_TE_reference(x, m) for x in line]
+
+        # Verificar que los resultados sean cercanos
+        assert np.allclose(result, real), f"Error en el modo TE m={m}"
+
+
+def test_TM_function(): 
+    """
+    Verifica que la función funcion_ondulatoria para los modos TM (Transverse Magnetic) 
+    genere resultados consistentes con una funcion hallada anteriormente en el Taller de Fotonica y Fibras opticas.
+    Esta funcion es f_TM_reference, y ya se basa en n_co, n_cl, h, k_0 dados
+
+    Se prueba para los tres primeros modos (m = 0, 1, 2).
+
+    Raises:
+        AssertionError: Si los valores calculados no coinciden con la referencia.
+    """
+
+    # Parámetros de la guía de onda
+    n_co = 1.5  # Índice de refracción del núcleo
+    n_cl = 1    # Índice de refracción del revestimiento
+    h = 1       # Altura del núcleo
+    k_0 = 2     # Número de onda en el vacío
+    modo = "TM" # Tipo de modo (Transverse Magnetic)
+
+    # Función que se hallo en el Taller de Fotonica y Fibras opticas
+    def f_TM_reference(U, m):
+        if m%2 == 0:
+            return ((n_cl/n_co)**2 * U*math.tan(U))**2 + U**2 - 1.25*math.pi**2
+        else:
+            return ((n_cl/n_co)**2 * U/math.tan(U))**2 + U**2 - 1.25*math.pi**2        
+
+    # Conjunto de valores en los que vamos a probar la funcion
+    # se evita el 0 porque se evalua en cotangente de U
+    line = np.linspace(0.001, math.pi/2, 50)
+
+    for m in range(3):
+        # Obtener la función generada por funcion_ondulatoria
+        f = funcion_ondulatoria(n_co=n_co, n_cl=n_cl, h=h, k_0=k_0, m=m, modo=modo)
+
+        # Evaluar la función generada y la referencia en el mismo rango
+        result = [f(x) for x in line]
+        real = [f_TM_reference(x, m) for x in line]
+
+        # Verificar que los resultados sean cercanos
+        assert np.allclose(result, real), f"Error en el modo TM m={m}"
+
+
+def test_result():
+    """
+    Prueba la función metodo_ondulatorio verificando que los valores obtenidos 
+    para los modos TE y TM sean cercanos a los valores de referencia.
+
+    La prueba compara los resultados calculados con valores de referencia 
+    para tres modos de propagación en una guía de onda plana.
+
+    Valores esperados:
+        - Modos TE: [75, 59.5, 43.8] grados
+        - Modos TM: [72.9, 55.5, 42.5] grados
+
+    Se utiliza una tolerancia de 0.1 a los valores de referencia
+    
+    Raises:
+        AssertionError: Si alguno de los valores calculados difieren por mas de 0.1 de los valores esperados.
+    """
+
+    # Valores de referencia obtenidos previamente
+    real_values_TE = [75, 59.5, 43.8]
+    real_values_TM = [72.9, 55.5, 42.5]
+
+    # Ejecutar el método para calcular los ángulos de los modos TE y TM
+    result = metodo_ondulatorio(
+        n_co=1.5, 
+        n_cl=1,
+        h=1,
+        k_0=2,
+        lambd=1,
+        ms=range(3) 
+    )
+
+    # Extraer los valores de TE y compararlos con los valores esperados
+    result_TE_array = list(result['TE'].values())
+    print("resultados py TE", result_TE_array)
+    assert np.allclose(result_TE_array,real_values_TE, atol=0.1), f"Error test result en el modo TE"
+
+    # Extraer los valores de TM y compararlos con los valores esperados
+    result_TM_array = list(result['TM'].values())
+    print("resultados py TM", result_TM_array)
+    assert np.allclose(result_TM_array, real_values_TM, atol=0.1), f"Error test result en el modo TM"
